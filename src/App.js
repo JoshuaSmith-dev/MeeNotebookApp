eacf72d03894a5e6a8fce307061223eacd372197
JoshuaSmith-dev <53170902+JoshuaSmith-dev@users.noreply.github.com>
2020-05-07 03:59:46 +0200

    reAddingFiles
    Adding Initial Files again

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React, {Component} from 'react';
-import Navbar from './components/Navbar';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Bio from './components/Bio';
-import Contact from './components/Contact';
-//import Todos from './Todos';
-//import AddTodo from './AddTodo';
-
+import Todos from './Todos';
+import AddTodo from './AddTodo';
 
 class App extends Component {
+  state = {
+    todos: [
+    {id: 1, content: 'buy some milk'},
+    {id: 2, content: 'play mario kart'}
+    ]
+  }
 
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+    this.setState({
+      todos: todos
+    })
+  }
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos: todos
+    })
+  }
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Navbar />
-          <Route path='/' component={Home} exact/>
-          <Route path='/bio' component={Bio} />
-          <Route path='/contact' component={Contact} />
-        </div>
-      </BrowserRouter>
-      
+      <div className="todo-app container"> 
+        <h1 className="center blue-text">Todos</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+        <AddTodo addTodo={this.addTodo}/>
+      </div>
     );
   }
 }
