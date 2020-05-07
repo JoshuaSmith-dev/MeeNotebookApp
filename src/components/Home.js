247d9f891bd83b3a0aeb3ca0517a8f2ed43c8b88
JoshuaSmith-dev <53170902+JoshuaSmith-dev@users.noreply.github.com>
2020-05-07 03:33:37 +0200

    adding InitialRoutes

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas officia corporis quis fuga, nemo a similique molestias expedita deserunt animi nobis nulla velit enim non harum sapiente. Laborum, quo neque!
+            </p>
+        </div>
+    )
+}
+
+export default Home;
